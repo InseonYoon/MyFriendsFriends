a6ba78779a3c7ae11661e83be94aabde4bc352f7
intotheseon <intotheseon@gmail.com>
2024-01-20 21:33:37 +0900

    Feat: add alert in .download and change theme color

---
+++ js/clickDownload.js
@@ -0,0 +1,11 @@
+const headerDL = document.querySelector('header .download');
+const mainDL = document.querySelector('main .download');
+
+headerDL.addEventListener('click', () => {
+    console.log('clicked')
+    alert('준비중입니다.')
+});
+mainDL.addEventListener('click', () => {
+    console.log('clicked')
+    alert('준비중입니다.')
+});
