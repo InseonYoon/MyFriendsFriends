5f2eb1ab28e7abfc13d9339ed011da1694cb65a1
intotheseon <intotheseon@gmail.com>
2024-01-23 02:30:45 +0900

    Feat: add marker on kakaomap

--- js/kakaomap.js
+++ js/kakaomap.js
@@ -6,13 +6,8 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
-var iwContent = '<div style="padding:5px;">남양주시동물보호센터</div>'
-iwPosition = new kakao.maps.LatLng(37.60878537279098, 127.19152396482963),
-iwRemoveable = true;
-
-// var infowindow = new kakao.maps.InfoWindow({
-//     map: map, // 인포윈도우가 표시될 지도
-//     position : iwPosition,
-//     content : iwContent,
-//     removable : iwRemoveable
-// });
+var markerPosition  = new kakao.maps.LatLng(37.60878537279098, 127.19152396482963);
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+marker.setMap(map);
