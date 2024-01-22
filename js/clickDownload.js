c910cc8cad8844ec03b9847ae8af68bbec49b69e
intotheseon <intotheseon@gmail.com>
2024-01-23 00:43:15 +0900

    Design: feat modal texts

--- js/clickDownload.js
+++ js/clickDownload.js
@@ -2,10 +2,8 @@ const headerDL = document.querySelector('header .download');
 const mainDL = document.querySelector('main .download');
 
 headerDL.addEventListener('click', () => {
-    console.log('clicked')
     alert('어림도 없지! 홍시 사진은 나만 가질거야...!')
 });
 mainDL.addEventListener('click', () => {
-    console.log('clicked')
     alert('어림도 없지! 홍시 사진은 나만 가질거야...!')
 });
